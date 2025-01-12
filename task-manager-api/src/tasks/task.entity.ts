0095af3c75dec10cb2159419a8101952b2cbbf0d
Zahaouma <marouane.zahaoui@eurafric-information.com>
2025-01-12 18:12:20 +0100

    add users and tasks entities

---
+++ task-manager-api/src/tasks/task.entity.ts
@@ -0,0 +1,39 @@
+import { User } from 'src/users/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ length: 50 })
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  priority: string;
+
+  @Column()
+  dueDate: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.tasks, { onDelete: 'CASCADE' })
+  user: User;
+}
---
+++ task-manager-api/src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import { Task } from 'src/tasks/task.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'int', default: 0 })
+  nombre_tasks: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
+}
